3a02b55c8bbcdd641dbabd186def4f23c7ba2b5c
Jarosław Kołosowski <jarokolosowski@gmail.com>
2023-06-03 07:39:26 +0200

    Add function getComputerChoice

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,1 +1,17 @@
-console.log("Working ...");
+function getComputerChoice() {
+  const index = Math.floor(Math.random() * 3);
+
+  if (index === 0) return "Rock";
+  if (index === 1) return "Paper";
+  if (index === 2) return "Scissors";
+}
+
+/* TESTS #################################################################### */
+
+/* testing getComputerChoice */
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
